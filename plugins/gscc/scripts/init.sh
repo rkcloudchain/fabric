13b5695513b218ec9a5d210e35ca68c59369862b
乔伦 徐 <jamesxql@gmail.com>
2018-11-17 20:30:40 +0800

    Modify Ubuntu apt source
    (cherry picked from commit 1df354be90326d41bd529d50a177218b33f976c2)

--- plugins/gscc/scripts/init.sh
+++ plugins/gscc/scripts/init.sh
@@ -6,6 +6,13 @@
 #
 # Update the entire system to the latest releases
 
+echo "deb http://mirrors.tencentyun.com/ubuntu/ xenial main restricted universe multiverse" > /etc/apt/sources.list
+echo "deb http://mirrors.tencentyun.com/ubuntu/ xenial-security main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://mirrors.tencentyun.com/ubuntu/ xenial-updates main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb-src http://mirrors.tencentyun.com/ubuntu/ xenial main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb-src http://mirrors.tencentyun.com/ubuntu/ xenial-security main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb-src http://mirrors.tencentyun.com/ubuntu/ xenial-updates main restricted universe multiverse" >> /etc/apt/sources.list
+
 apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
