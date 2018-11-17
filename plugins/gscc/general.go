f81291699967826ba8046d12995339aba4301f11
乔伦 徐 <jamesxql@gmail.com>
2018-11-17 19:20:33 +0800

    Add GetState API
    (cherry picked from commit bd1a25098b0a893e9609641f9b32ff0046a355a6)

--- plugins/gscc/general.go
+++ plugins/gscc/general.go
@@ -7,8 +7,12 @@ SPDX-License-Identifier: Apache-2.0
 package main
 
 import (
+	"fmt"
+
 	"github.com/hyperledger/fabric/common/flogging"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/core/ledger"
+	"github.com/hyperledger/fabric/core/peer"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
@@ -36,7 +40,46 @@ func (s *GeneralQuerier) Init(stub shim.ChaincodeStubInterface) pb.Response {
 
 // Invoke implements the chaincode shim interface
 func (s *GeneralQuerier) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	return shim.Success(nil)
+	cid := stub.GetChannelID()
+	logger.Infof("Channel ID: %s", cid)
+
+	args := stub.GetArgs()
+	if len(args) < 2 {
+		return shim.Error(fmt.Sprintf("Incorrect number of arguments, %d", len(args)))
+	}
+	chaincodeID := string(args[0])
+	fname := string(args[1])
+
+	targetLedger := peer.GetLedger(cid)
+	if targetLedger == nil {
+		return shim.Error(fmt.Sprintf("Invalid chain ID, %s", cid))
+	}
+
+	switch fname {
+	case GetState:
+		if len(args) < 3 {
+			return shim.Error(fmt.Sprintf("missing 3rd argument for %s", fname))
+		}
+		executor, err := targetLedger.NewQueryExecutor()
+		if err != nil {
+			return shim.Error(fmt.Sprintf("Failed to get QueryExecutor with cid %s, error %s", cid, err))
+		}
+		return getState(executor, chaincodeID, args[2])
+	}
+
+	return shim.Error(fmt.Sprintf("Requested function %s not found.", fname))
+}
+
+func getState(executor ledger.QueryExecutor, chaincodeID string, key []byte) pb.Response {
+	if key == nil {
+		return shim.Error("State key must not be nil")
+	}
+
+	value, err := executor.GetState(chaincodeID, string(key))
+	if err != nil {
+		return shim.Error(fmt.Sprintf("Failed to get state with namespace %s, error %s", chaincodeID, err))
+	}
+	return shim.Success(value)
 }
 
 func main() {}
