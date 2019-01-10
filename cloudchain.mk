6baada30b018cdfde06d89737684e58cb4e31e22
Xu Qiaolun <jamesxql@gmail.com>
2019-01-10 14:39:39 +0800

    Update BASE_VERSION and BASEIMAGE_RELEASE

--- cloudchain.mk
+++ cloudchain.mk
@@ -3,8 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-BASE_VERSION = 1.3.1
-BASEIMAGE_RELEASE=0.4.13
+BASE_VERSION = 1.4.0
+BASEIMAGE_RELEASE=0.4.14
 BASE_DOCKER_LABEL = org.hyperledger.fabric
 
 BUILD_DIR ?= .build
