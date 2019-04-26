e36faeb46ba7dd3636ac726dd122c6218fbaa6ea
Xu Qiaolun <jamesxql@gmail.com>
2019-04-26 14:57:31 +0800

    Support http/2 protocol

--- plugins/chconfig/start/start.go
+++ plugins/chconfig/start/start.go
@@ -1,6 +1,7 @@
 package start
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net"
 	"net/http"
@@ -13,6 +14,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"golang.org/x/net/http2"
 )
 
 var logger = flogging.MustGetLogger("chconfig.start")
@@ -47,15 +49,18 @@ func addFlags(cmd *cobra.Command) {
 
 	flags.String("hostname", "0.0.0.0", "The hostname or IP on which the REST server will listen")
 	flags.Int("port", 8059, "The port on which the REST server will listen")
+	flags.Bool("server.tls", false, "Use TLS when communicating with the server")
+	flags.String("server.tls.keyfile", "", "Path to file of the private key of the TLS certificate")
+	flags.String("server.tls.certfile", "", "Path to file of the server TLS certificate")
 	flags.String("mspdir", "msp", "The MSP path")
 	flags.String("mspid", "SampleOrg", "The MSP id")
 	flags.StringP("orderer", "o", "", "Ordering service endpoint")
-	flags.Bool("tls", false, "Use TLS when communicating with the orderer endpoint")
-	flags.String("cafile", "", "Path to file containing PEM-encoded trusted certificate(s) for the ordering endpoint")
+	flags.Bool("orderer.tls", false, "Use TLS when communicating with the orderer endpoint")
+	flags.String("orderer.cafile", "", "Path to file containing PEM-encoded trusted certificate(s) for the ordering endpoint")
 	flags.Duration("connTimeout", 3*time.Second, "Timeout for client to connect")
-	flags.Bool("clientauth", false, "Use mutual TLS when communicating with the orderer endpoint")
-	flags.String("keyfile", "", "Path to file containing PEM-encoded private key to use for mutual TLS communication with the orderer endpoint")
-	flags.String("certfile", "", "Path to file containing PEM-encoded X509 public key to use for mutual TLS communication with the orderer endpoint")
+	flags.Bool("orderer.clientauth", false, "Use mutual TLS when communicating with the orderer endpoint")
+	flags.String("orderer.keyfile", "", "Path to file containing PEM-encoded private key to use for mutual TLS communication with the orderer endpoint")
+	flags.String("orderer.certfile", "", "Path to file containing PEM-encoded X509 public key to use for mutual TLS communication with the orderer endpoint")
 
 	bindFlags(flags)
 }
@@ -63,15 +68,18 @@ func addFlags(cmd *cobra.Command) {
 func bindFlags(flags *pflag.FlagSet) {
 	bindFlag("listen.address", "hostname", flags)
 	bindFlag("listen.port", "port", flags)
+	bindFlag("server.tls.enabled", "server.tls", flags)
+	bindFlag("server.tls.keyfile", "server.tls.keyfile", flags)
+	bindFlag("server.tls.certfile", "server.tls.certfile", flags)
 	bindFlag("msp.path", "mspdir", flags)
 	bindFlag("msp.id", "mspid", flags)
 	bindFlag("orderer.address", "orderer", flags)
-	bindFlag("orderer.tls.enabled", "tls", flags)
-	bindFlag("orderer.tls.rootcert.file", "cafile", flags)
+	bindFlag("orderer.tls.enabled", "orderer.tls", flags)
+	bindFlag("orderer.tls.rootcert.file", "orderer.cafile", flags)
 	bindFlag("orderer.client.connTimeout", "connTimeout", flags)
-	bindFlag("orderer.tls.clientAuthRequired", "clientauth", flags)
-	bindFlag("orderer.tls.clientKey.file", "keyfile", flags)
-	bindFlag("orderer.tls.clientCert.file", "certfile", flags)
+	bindFlag("orderer.tls.clientAuthRequired", "orderer.clientauth", flags)
+	bindFlag("orderer.tls.clientKey.file", "orderer.keyfile", flags)
+	bindFlag("orderer.tls.clientCert.file", "orderer.certfile", flags)
 }
 
 func bindFlag(key, name string, flags *pflag.FlagSet) {
@@ -83,18 +91,44 @@ func bindFlag(key, name string, flags *pflag.FlagSet) {
 }
 
 func startServer(address string) error {
-	listener, err := net.Listen("tcp", address)
+	var listener net.Listener
+	var tlscfg *tls.Config
+	var err error
+
+	listener, err = net.Listen("tcp", address)
 	if err != nil {
 		return err
 	}
-
 	logger.Infof("Serving HTTP requests on %s", listener.Addr())
-	err = http.Serve(listener, router())
-	if err != nil {
-		logger.Errorf("Error starting server:[%s]\n", err)
-		return err
+
+	tlsEnabled := viper.Get("server.tls.enabled").(bool)
+	if tlsEnabled {
+		cert, err := tls.LoadX509KeyPair(viper.Get("server.tls.certfile").(string), viper.Get("server.tls.keyfile").(string))
+		if err != nil {
+			return err
+		}
+
+		tlscfg = &tls.Config{Certificates: []tls.Certificate{cert}}
+		tlscfg.NextProtos = []string{"h2", "http/1.1"}
+		listener = tls.NewListener(listener, tlscfg)
 	}
 
+	srv := &http.Server{
+		Addr:      listener.Addr().String(),
+		TLSConfig: tlscfg,
+		Handler:   router(),
+	}
+	if tlsEnabled {
+		err = http2.ConfigureServer(srv, nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = srv.Serve(listener)
+	if err != nil && err != http.ErrServerClosed {
+		return err
+	}
 	return nil
 }
 
